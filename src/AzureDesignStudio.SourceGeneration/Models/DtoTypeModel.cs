6a0d0e63549d9b17773338c1a7188f9ccf07b99b
Chun Liu <chunliu@microsoft.com>
2022-11-30 15:39:38 +0800

    Use Scriban template for source gen.

---
+++ src/AzureDesignStudio.SourceGeneration/Models/DtoTypeModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AzureDesignStudio.SourceGeneration.Models
+{
+    internal class DtoTypeProperty
+    {
+        public string Identifier { get; set; } = default!;
+        public string Type { get; set; } = default!;
+    }
+    internal class DtoTypeModel
+    {
+        public string Namespace { get; set; } = default!;
+        public string ClassName { get; set; } = default!;
+        public IList<string> Usings { get; set; } = default!;
+        public IList<DtoTypeProperty> Properties { get; set; } = default!;
+    }
+}
---
+++ src/AzureDesignStudio.SourceGeneration/ResourceReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AzureDesignStudio.SourceGeneration
+{
+    internal class ResourceReader
+    {
+        public static string? GetResource<TAssembly>(string endWith) => GetResource(endWith, typeof(TAssembly));
+
+        public static string? GetResource(string endWith, Type? assemblyType = null)
+        {
+            var assembly = GetAssembly(assemblyType);
+
+            var resources = assembly.GetManifestResourceNames().Where(r => r.EndsWith(endWith));
+
+            if (!resources.Any()) throw new InvalidOperationException($"There is no resources that ends with '{endWith}'");
+            if (resources.Count() > 1) throw new InvalidOperationException($"There is more then one resource that ends with '{endWith}'");
+
+            var resourceName = resources.Single();
+
+            return ReadEmbededResource(assembly, resourceName);
+        }
+
+        private static Assembly GetAssembly(Type? assemblyType)
+        {
+            Assembly assembly;
+            if (assemblyType == null)
+            {
+                assembly = Assembly.GetExecutingAssembly();
+            }
+            else
+            {
+                assembly = Assembly.GetAssembly(assemblyType);
+            }
+
+            return assembly;
+        }
+
+        private static string? ReadEmbededResource(Assembly assembly, string name)
+        {
+            using var resourceStream = assembly.GetManifestResourceStream(name);
+            if (resourceStream == null) return null;
+            using var streamReader = new StreamReader(resourceStream);
+            return streamReader.ReadToEnd();
+        }
+    }
+}
